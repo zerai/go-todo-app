2b07a5d092086141ea06e6f4e342ccb0acc2b6f9
zerai <teclai.zerai@gmail.com>
2020-07-04 13:21:12 +0200

    TodoRepository: add method add().

--- todo/todo_repository.go
+++ todo/todo_repository.go
@@ -4,20 +4,54 @@ import (
 	"errors"
 )
 
-var ErrTodoNotFound = errors.New("Todo not found")
+var (
+	// ErrTodoNotFound ...
+	ErrTodoNotFound = errors.New("Todo not found")
 
+	// ErrAlreadyExist ...
+	ErrAlreadyExist = errors.New("Todo already exist")
+)
+
+// TodoRepository interface for TodoRepository
 type TodoRepository interface {
 	findByID(identifier int) Todo
 }
 
+// TodoRepositoryInMemory in memory storage for Todo
 type TodoRepositoryInMemory struct {
 	todos map[int]Todo
 }
 
+// NewTodoRepositoryInMemory factory method
+func NewTodoRepositoryInMemory() *TodoRepositoryInMemory {
+
+	return &TodoRepositoryInMemory{
+		todos: make(map[int]Todo),
+	}
+}
+
+// FindByID find Todo by identifier, returns Todo or error
 func (r TodoRepositoryInMemory) FindByID(identifier int) (Todo, error) {
 	todo, ok := r.todos[identifier]
 	if !ok {
 		return Todo{}, ErrTodoNotFound
 	}
 	return todo, nil
+}
+
+// Add add a Todo to repository
+func (r *TodoRepositoryInMemory) Add(aTodo Todo) error {
+	key := aTodo.ID()
+	_, err := r.FindByID(key)
+
+	switch err {
+	case ErrTodoNotFound:
+		r.todos[key] = aTodo
+	case nil:
+		return ErrAlreadyExist
+	default:
+		return err
+	}
+
+	return nil
 }
--- todo/todo_repository_test.go
+++ todo/todo_repository_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestTodoRepository(t *testing.T) {
 
-	TodoRepositoryInMemory := TodoRepositoryInMemory{
+	todoRepositoryInMemory := TodoRepositoryInMemory{
 		todos: map[int]Todo{
 			123: {123, "a label", "a description"},
 			321: {321, "a second label", "a second description"},
@@ -14,7 +14,7 @@ func TestTodoRepository(t *testing.T) {
 	}
 
 	t.Run("findByID should returns a Todo", func(t *testing.T) {
-		got, err := TodoRepositoryInMemory.FindByID(123)
+		got, err := todoRepositoryInMemory.FindByID(123)
 		want := Todo{123, "a label", "a description"}
 
 		if err != nil {
@@ -27,11 +27,40 @@ func TestTodoRepository(t *testing.T) {
 	})
 
 	t.Run("findByID should returns error", func(t *testing.T) {
-		_, got := TodoRepositoryInMemory.FindByID(555)
+		_, got := todoRepositoryInMemory.FindByID(555)
 
 		assertError(t, got, ErrTodoNotFound)
 	})
 
+}
+
+func TestAddTodo(t *testing.T) {
+	t.Run("Should add a valid todo", func(t *testing.T) {
+		emptytodoRepositoryInMemory := NewTodoRepositoryInMemory()
+		aNewTodo := NewTodoAsValue(555, "a label")
+
+		emptytodoRepositoryInMemory.Add(aNewTodo)
+
+		got, err := emptytodoRepositoryInMemory.FindByID(555)
+		want := aNewTodo
+		if err != nil {
+			t.Fatal("should find added Todo:", err)
+		}
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Should returns error if Todo exist", func(t *testing.T) {
+		repository := NewTodoRepositoryInMemory()
+		aNewTodo := NewTodoAsValue(99, "a label")
+		repository.Add(aNewTodo)
+
+		err := repository.Add(aNewTodo)
+
+		assertError(t, err, ErrAlreadyExist)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
