e3e61f743cbd2f42d1768b9b82a3eded62a49f84
zerai <teclai.zerai@gmail.com>
2020-07-05 17:22:32 +0200

    Improve TodoServer.

--- todo_server.go
+++ todo_server.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // TodoServer currently returns 'TODO ONE' given _any_ request.
 func TodoServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "TODO ONE")
+	todoId := strings.TrimPrefix(r.URL.Path, "/todos/")
+
+	fmt.Fprint(w, GetTodoData(todoId))
+}
+
+func GetTodoData(todoId string) string {
+	if todoId == "123" {
+		return "123 - first todo"
+	}
+
+	if todoId == "666" {
+		return "666 - first todo"
+	}
+
+	return ""
 }
--- todo_server_test.go
+++ todo_server_test.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestGETTodos(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
 
-	TodoServer(response, request)
+	t.Run("returns todo data for ID 123", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/todos/123", nil)
+		response := httptest.NewRecorder()
 
-	t.Run("returns a todo data", func(t *testing.T) {
-		got := response.Body.String()
-		want := "TODO ONE"
+		TodoServer(response, request)
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "123 - first todo")
 	})
 
+	t.Run("returns todo data for ID 666", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/todos/666", nil)
+		response := httptest.NewRecorder()
+
+		TodoServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "666 - first todo")
+	})
+
+}
+
+func newGetTodoRequest(todoId string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", todoId), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
 }
